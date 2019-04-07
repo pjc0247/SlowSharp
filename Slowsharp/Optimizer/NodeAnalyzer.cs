6746dd77fb62c4a98b8fe5fee7f5646e56b30338
Park June Chul <pjc0247@naver.com>
2019-04-07 15:30:55 +0900

    [WIP] Optmizer

---
+++ Slowsharp/Optimizer/NodeAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Slowsharp
+{
+    internal class NodeAnalyzer
+    {
+        public bool IsStaticExpression(ExpressionSyntax node)
+        {
+            if (node is LiteralExpressionSyntax)
+                return true;
+            if (node is BinaryExpressionSyntax bin)
+                return IsStaticExpression(bin.Left) && IsStaticExpression(bin.Right);
+            if (node is DefaultExpressionSyntax)
+                return true;
+            if (node is SizeOfExpressionSyntax)
+                return true;
+
+            return false;
+        }
+
+        public bool TrackChanges(SyntaxNode node)
+        {
+            return false;
+        }
+    }
+}
