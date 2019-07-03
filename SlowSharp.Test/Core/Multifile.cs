73b250a111f893aadd7fb80b8a7a658e75acf78e
Park June Chul <pjc0247@naver.com>
2019-07-03 11:28:40 +0900

    add multifile test

---
+++ SlowSharp.Test/Core/Multifile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Slowsharp.Test
+{
+    [TestClass]
+    public class MultifileTest
+    {
+        [TestMethod]
+        public void Multifile()
+        {
+            var r = CScript.CreateRunner(new string[]{
+                @"
+class Bar { public int GiveMeNumber() => 10; }
+",
+            @"
+class Foo { public int GiveMeNumber() => (new Bar()).GiveMeNumber() * 2; }
+"});
+            var foo = r.Instantiate("Foo");
+            Assert.AreEqual(20, foo.Invoke("GiveMeNumber").Unwrap());
+        }
+    }
+}
