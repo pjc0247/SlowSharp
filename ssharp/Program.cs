5859b08dd2d5b730b8d8c5bb991b92ee617bc74b
Park June Chul <pjc0247@naver.com>
2019-07-05 17:46:35 +0900

    add 'ssharp' cli

---
+++ ssharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Slowsharp;
+
+namespace ssharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Please provide input file.");
+                    Console.WriteLine("   ssharp a.cs b.cs c.cs");
+                    return;
+                }
+
+                var srcs = new List<string>();
+                foreach (var arg in args)
+                    srcs.Add(File.ReadAllText(arg));
+
+                var cs = CScript.CreateRunner(srcs.ToArray());
+                cs.RunMain();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ ssharp/readme.md
@@ -0,0 +1,7 @@
+ssharp
+====
+slowsharp cli for demonstrate how it works.
+
+```
+ssharp input.csx
+```
