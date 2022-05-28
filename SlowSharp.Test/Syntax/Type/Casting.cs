d4468ca4b8d686bf8a7172151e9cdaa61579f009
Park June Chul <pjc0247@naver.com>
2022-05-28 10:50:12 +0900

    test: casting

---
+++ SlowSharp.Test/Syntax/Type/Casting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Slowsharp.Test
+{
+    [TestClass]
+    public class CastingTest
+    {
+        [TestMethod]
+        public void Primitive()
+        {
+            Assert.AreEqual(
+                typeof(float).Name,
+                TestRunner.Run(@"return (float)1;").GetType().Name);
+
+            Assert.AreEqual(
+                typeof(int).Name,
+                TestRunner.Run(@"return (int)1.0f;").GetType().Name);
+        }
+
+        [TestMethod]
+        public void IllegalCasting()
+        {
+            // FIXME
+            // this should throw an exception.
+            /*
+            Assert.AreEqual(
+                typeof(float).Name,
+                TestRunner.Run(@"return (string)1;").GetType().Name);
+            */
+        }
+    }
+}
