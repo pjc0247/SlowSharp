2b0d0ba647700936f8a6a83d14b3bbe6cf6eef25
Park June Chul <pjc0247@naver.com>
2022-06-04 02:33:45 +0900

    test: Array

---
+++ SlowSharp.Test/Syntax/Array/Array.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Slowsharp.Test
+{
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void ArrayType()
+        {
+            Assert.AreEqual(typeof(int[]), TestRunner.Run(@"
+var emptyIntAry = new int[] { };
+return emptyIntAry.GetType();
+"));
+
+            Assert.AreEqual(typeof(string[]), TestRunner.Run(@"
+var emptyStrAry = new string[] { };
+return emptyStrAry.GetType();
+"));
+
+            Assert.AreEqual(typeof(object[]), TestRunner.Run(@"
+var emptyObjAry = new object[] { };
+return emptyObjAry.GetType();
+"));
+        }
+
+        [TestMethod]
+        public void CreateWithTrailingElem()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, TestRunner.Run(@"
+var ary = new int[] { 1,2,3,4,5 };
+return ary;
+") as ICollection);
+
+            CollectionAssert.AreEqual(new string[] { "A", "B", "C" }, TestRunner.Run(@"
+var ary = new string[] { ""A"", ""B"", ""C"" };
+return ary;
+") as ICollection);
+
+            CollectionAssert.AreEqual(new object[] { 1,2,3, "A", "B", "C" }, TestRunner.Run(@"
+var ary = new object[] { 1,2,3, ""A"", ""B"", ""C"" };
+return ary;
+") as ICollection);
+        }
+    }
+}
