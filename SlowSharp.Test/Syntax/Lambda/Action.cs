5c1f615ca54fcc31e0705d2160c00e2fc6912bd8
Park June Chul <pjc0247@naver.com>
2022-07-03 23:30:54 +0900

    test:

---
+++ SlowSharp.Test/Syntax/Lambda/Action.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Slowsharp.Test
+{
+    [TestClass]
+    public class LambdaActionTest
+    {
+        [TestMethod]
+        public void BasicCreation()
+        {
+            Assert.AreEqual(
+                typeof(Action),
+                TestRunner.Run(@"return new Action(() => { }").GetType());
+
+            Assert.AreEqual(
+                typeof(Action<int>),
+                TestRunner.Run(@"return new Action((int p) => { }").GetType());
+        }
+
+        [TestMethod]
+        public void Invocation()
+        {
+            Assert.AreEqual(
+                10,
+                TestRunner.Run(@"
+var b = 0;
+var a = new Action(() => {
+    b = 10;
+});
+a();
+return b;
+"));
+        }
+    }
+}
---
+++ SlowSharp.Test/Syntax/Lambda/Func.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Slowsharp.Test
+{
+    [TestClass]
+    public class LambdaFuncTest
+    {
+        [TestMethod]
+        public void BasicCreation()
+        {
+            Assert.AreEqual(
+                typeof(Func<int>),
+                TestRunner.Run(@"return new Func(() => { return 1; }").GetType());
+
+            Assert.AreEqual(
+                typeof(Func<int, int>),
+                TestRunner.Run(@"return new Func((int p) => { return 1; }").GetType());
+        }
+
+        [TestMethod]
+        public void Invocation()
+        {
+            Assert.AreEqual(
+                10,
+                TestRunner.Run(@"
+var a = new Func(() => {
+    return 10;
+});
+return a();
+"));
+        }
+    }
+}
